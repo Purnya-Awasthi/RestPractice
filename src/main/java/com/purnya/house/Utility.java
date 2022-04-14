aa8543caf4ddf6e8aba7bebf9cc3b54e873289df
Purnya Awasthi <pa1039@wildcats.unh.edu>
2022-04-14 05:56:42 -0400

    initial commit of REST API

---
+++ src/main/java/com/purnya/house/Utility.java
@@ -0,0 +1,15 @@
+package com.purnya.house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Utility {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+SpringApplication.run(Utility.class, args);
+	}
+
+}
